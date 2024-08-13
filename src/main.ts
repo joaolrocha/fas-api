f5d579ae9a6f5388d44e3b45c8cefa27a179ebfa
joaolrocha <joaolrocha07@gmail.com>
2024-08-13 16:05:53 -0300

    fix: cors origin

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // ou '*' para permitir todas as origens
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3001);
 }
 bootstrap();
