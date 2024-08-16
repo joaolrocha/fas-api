723f7e2dbf8b842f68558cf095eafa83015d8242
joaolrocha <joaolrocha07@gmail.com>
2024-08-16 11:22:24 -0300

    feat: add env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientModule } from './client/client.module';
 import { ConsultorModule } from './consultor/consultor.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'br622.hostgator.com.br',  // substitua pelo host do seu banco
-      port: 3306,         // substitua pela porta do seu banco
-      username: 'fassol82_fas_portal', // substitua pelo usuário do banco
-      password: 'Fas@2024', // substitua pela senha do banco
-      database: 'fassol82_fas_portal', // substitua pelo nome do banco
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // use com cuidado em produção; pode criar/migrar tabelas automaticamente
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna o módulo de configuração global
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // use com cuidado em produção; pode criar/migrar tabelas automaticamente
+      }),
     }),
     ClientModule,
     ConsultorModule,
